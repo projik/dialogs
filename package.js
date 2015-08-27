3646e14ae5675c576dab9ed9beb1b9aaefd54fe5
Andrew Reedy <andrewrreedy@gmail.com>
2015-08-27 13:12:10 -0700

    update

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: "hybrid:dialogs",
+  name: "hybrid:dialogss",
   summary: "Cross-platform dialogs: alerts, confirms, and prompts (Cordova).",
   version: "1.0.2",
   git: "https://github.com/meteorhybrid/dialogs"
 });
 
 Cordova.depends({
-  "org.apache.cordova.dialogs":"0.2.8"
+  "org.apache.cordova.dialogs":"0.3.0"
 });
 
 Package.onUse(function(api) {
   api.export('Dialogs');
   api.versionsFrom('METEOR@1.0');
 
-  api.addFiles('dialogs.js');
+  api.addFiles('dialogs.js', 'client');
   api.addFiles('dialogs-browser.js', ['web.browser']);
   api.addFiles('dialogs-cordova.js', ['web.cordova']);
 });
