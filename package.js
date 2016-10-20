975306be6f11932c66a71daeb8423876bd00309d
David Baxter <davebaxter@gmail.com>
2016-10-21 10:47:58 +1100

    Update for latest version of cordova plugin

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: "hybrid:dialogs",
+  name: "supaseca:dialogs",
   summary: "Cross-platform dialogs: alerts, confirms, and prompts (Cordova).",
-  version: "1.0.3",
-  git: "https://github.com/meteorhybrid/dialogs"
+  version: "1.0.4",
+  git: "https://github.com/projik/dialogs"
 });
 
 Cordova.depends({
-  "org.apache.cordova.dialogs":"0.3.0"
+  "org.apache.cordova.dialogs":"1.3.0"
 });
 
 Package.onUse(function(api) {
   api.export('Dialogs');
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.3');
 
   api.addFiles('dialogs.js', 'client');
   api.addFiles('dialogs-browser.js', ['web.browser']);
